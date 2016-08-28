deaa3175d554585170ce8e059141439680a79a66
Robb Shecter <robb@weblaws.org>
2016-08-28 15:19:33 -0700

    refactoring

--- lib/string_ops.rb
+++ lib/string_ops.rb
@@ -6,20 +6,22 @@ module StringOps
   end
 
 
-  def fix_whitespace(s)
-    s.gsub "\n", " "
+  def fix_whitespace(a_string)
+    a_string.gsub "\n", " "
   end
 
 
-  def fix_hyphenation(s)
-    s.gsub "- ", ""
+  def fix_hyphenation(a_string)
+    a_string.gsub "- ", ""
   end
 
-  def split_into_sentences(s)
-    s
+
+  def split_into_sentences(a_string)
+    a_string
       .split(". ")
       .map { |sentence| ensure_ends_with_period(sentence) }
   end
+
 
   def ensure_ends_with_period(sentence)
     sentence + (sentence.end_with?(".") ? "" : ".")
