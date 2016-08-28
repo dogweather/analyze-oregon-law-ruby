56b341b5e2c6d440479984a38996792f324082e6
Robb Shecter <robb@weblaws.org>
2016-08-28 15:14:36 -0700

    #cleanup

--- lib/string_ops.rb
+++ lib/string_ops.rb
@@ -1,11 +1,27 @@
 module StringOps
   module_function
 
-  def fix_whitespace(in_string:)
-    in_string.gsub "\n", ' '
+  def clean_up(summary:)
+    split_into_sentences(fix_hyphenation(fix_whitespace(summary))).first
   end
 
-  def fix_hyphenation(in_string:)
-    in_string.gsub '- ', ''
+
+  def fix_whitespace(s)
+    s.gsub "\n", " "
+  end
+
+
+  def fix_hyphenation(s)
+    s.gsub "- ", ""
+  end
+
+  def split_into_sentences(s)
+    s
+      .split(". ")
+      .map { |sentence| ensure_ends_with_period(sentence) }
+  end
+
+  def ensure_ends_with_period(s)
+    s.end_with?(".") ? s : s + "."
   end
 end
--- spec/string_ops_spec.rb
+++ spec/string_ops_spec.rb
@@ -4,13 +4,21 @@ include StringOps
 describe StringOps do
   describe '#fix_whitespace' do
     it 'changes newline to a blank' do
-      expect( fix_whitespace in_string: "new\nline" ).to eq 'new line'
+      expect( fix_whitespace "new\nline" ).to eq 'new line'
     end
   end
 
   describe '#fix_hyphenation' do
     it 'de-hyphenates' do
-      expect( fix_hyphenation in_string: 'zip- per' ).to eq 'zipper'
+      expect( fix_hyphenation 'zip- per' ).to eq 'zipper'
     end
   end
+
+  describe '#clean_up' do
+    it "handles extra text" do
+      input = "Relating to the state transient lodging tax; creating\nnew provisions; amending ORS 284.131 and\n320.305; prescribing an effective date; and pro-\nviding for revenue raising that requires approval\nby a three-fifths majority.\nWhereas Enrolled House Bill 2267 (chapter 818,"
+      expect( clean_up summary: input ).to eq "Relating to the state transient lodging tax; creating new provisions; amending ORS 284.131 and 320.305; prescribing an effective date; and providing for revenue raising that requires approval by a three-fifths majority."
+    end
+  end
+
 end
